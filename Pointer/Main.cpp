abb5c245d8f82e3c3e888ce606ee7370c67c27a4
jae <wod6983@gmail.com>
2026-01-21 09:59:18 +0900

    Pointer, Memory Leak 검사 예제 진행
    - crt 라이브러리를 활용해 메모리 릭 검사하는 예제 진행

--- Pointer/Main.cpp
+++ Pointer/Main.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 
-void DeletePointer(void*& ptr)
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#define new new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+// allocations to be of _CLIENT_BLOCK type
+#else
+#define DBG_NEW new
+#endif
+
+void DeletePointer(void** ptr)
 {
-    if (ptr != nullptr)
+    if (*ptr != nullptr)
     {
-        delete ptr;
-        ptr = nullptr;
+        delete *ptr;
+        *ptr = nullptr;
     }
 }
 
 int main()
 {
-    //동적 할당
-    char* buffer = new char[10];
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    //_CrtSetBreakAlloc(159);
+    // 
+    //TODO: 메모리 릭
+    int* intPointer = new int; //ms compiler기준으로 int는 4바이트가 맞습니다.
+    delete intPointer;
+
+    int a = 1;
     //포인터 변수 선언 및 초기화
     //void* pointer = nullptr;
     void* pointer = new int;
-    DeletePointer(pointer);
+
+
+    DeletePointer(&pointer);
     //delete pointer;
     //pointer = nullptr;
     //null check
@@ -28,6 +48,6 @@ int main()
     {
         std::cout << "Pointer is null\n";
     }
-
-    std::cin.get();
+    //_CrtDumpMemoryLeaks();
+    //std::cin.get();
 }
